ce0bcf801214ebab5f6f5a3360affa4f2730bf82
sevillalarry <sevilla.larry.oss@gmail.com>
2022-11-09 09:21:33 +0800

    Create b.5.06.Libstdc++.from.GCC-12.2.0.sh

---
+++ 11.2/b.5.06.Libstdc++.from.GCC-12.2.0.sh
@@ -0,0 +1,30 @@
+# b.5.06.Libstdc++.from.GCC-12.2.0.sh
+#
+
+tar xvf gcc-12.2.0.tar.xz
+cd gcc-12.2.0
+
+time { \
+\
+mkdir -v build && \
+cd       build && \
+\
+../libstdc++-v3/configure           \
+    --host=$LFS_TGT                 \
+    --build=$(../config.guess)      \
+    --prefix=/usr                   \
+    --disable-multilib              \
+    --disable-nls                   \
+    --disable-libstdcxx-pch         \
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/12.2.0  && \
+\
+make && \
+\
+make DESTDIR=$LFS install && \
+\
+rm -v $LFS/usr/lib/lib{stdc++,stdc++fs,supc++}.la \
+\
+; }
+
+cd ..
+rm -rf gcc-12.2.0
