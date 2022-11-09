b8f8ef8e1426afabb860add9cd9d595342945432
sevillalarry <sevilla.larry.oss@gmail.com>
2022-11-09 11:07:32 +0800

    Create b.6.17.Binutils-2.39-Pass2.sh

---
+++ 11.2/b.6.17.Binutils-2.39-Pass2.sh
@@ -0,0 +1,35 @@
+# b.6.17.Binutils-2.39-Pass2.sh
+#
+
+export PKG="binutils-2.39"
+tar xvf $PKG.tar.xz
+cd $PKG
+
+time { \
+\
+sed '6009s/$add_dir//' -i ltmain.sh       && \
+\
+mkdir -v build  && \
+cd       build  && \
+\
+../configure                   \
+    --prefix=/usr              \
+    --build=$(../config.guess) \
+    --host=$LFS_TGT            \
+    --disable-nls              \
+    --enable-shared            \
+    --enable-gprofng=no        \
+    --disable-werror           \
+    --enable-64-bit-bfd     && \
+\
+make && \
+\
+make DESTDIR=$LFS install   && \
+\
+rm -v $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes}.{a,la}  \
+\
+; }
+
+cd ..
+rm -rf $PKG
+unset PKG
