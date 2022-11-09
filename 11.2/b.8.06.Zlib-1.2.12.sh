e9cd0bbbe08aafb640461d64f3dca725f50819d0
sevillalarry <sevilla.larry.oss@gmail.com>
2022-11-09 16:47:52 +0800

    Create b.8.06.Zlib-1.2.12.sh

---
+++ 11.2/b.8.06.Zlib-1.2.12.sh
@@ -0,0 +1,24 @@
+# b.8.06.Zlib-1.2.12.sh
+#
+
+export PKG="zlib-1.2.12"
+tar xvf $PKG.tar.xz
+cd $PKG
+
+time { \
+\
+./configure --prefix=/usr && \
+\
+make          && \
+\
+make check    && \
+\
+make install  && \
+\
+rm -fv /usr/lib/libz.a  \
+\
+; }
+
+cd ..
+rm -rf $PKG
+unset PKG
