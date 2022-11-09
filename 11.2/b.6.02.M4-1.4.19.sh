35156712ece05d47ec813dfafd89e879936a52f5
sevillalarry <sevilla.larry.oss@gmail.com>
2022-11-09 09:26:39 +0800

    Update b.6.02.M4-1.4.19.sh

--- 11.2/b.6.02.M4-1.4.19.sh
+++ 11.2/b.6.02.M4-1.4.19.sh
@@ -1,1 +1,20 @@
+# b.6.02.M4-1.4.19.sh
+#
 
+tar xvf m4-1.4.19.tar.xz
+cd m4-1.4.19
+
+time { \
+\
+./configure --prefix=/usr   \
+            --host=$LFS_TGT \
+            --build=$(build-aux/config.guess)  && \
+\
+make && \
+\
+make DESTDIR=$LFS install \
+\
+; }
+
+cd ..
+rm -rf m4-1.4.19
