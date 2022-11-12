e046f30f9189e518401d939b2ab48e7c12fd2071
sevillalarry <sevilla.larry.oss@gmail.com>
2022-11-12 12:54:41 +0800

    LibStdC++ logging

--- 11.2/b.5.06.Libstdc++.from.GCC-12.2.0.sh
+++ 11.2/b.5.06.Libstdc++.from.GCC-12.2.0.sh
@@ -2,7 +2,17 @@
 #
 
 export PKG="gcc-12.2.0"
-tar xvf $PKG.tar.xz
+export PKGLOG_DIR=$LFSLOG/5.06
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+tar xvf $PKG.tar.xz > $PKGLOG_TAR 2> $PKGLOG_ERROR
 cd $PKG
 
 time { \
@@ -10,6 +20,7 @@ time { \
 mkdir -v build      && \
 cd       build      && \
 \
+echo "2. Configure ..."         && \
 ../libstdc++-v3/configure           \
     --host=$LFS_TGT                 \
     --build=$(../config.guess)      \
@@ -17,11 +28,15 @@ cd       build      && \
     --disable-multilib              \
     --disable-nls                   \
     --disable-libstdcxx-pch         \
-    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/12.2.0  && \
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/12.2.0   \
+            > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR  && \
 \
-make                        && \
+echo "3. Make Build ..."                        && \
+make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR          && \
 \
-make DESTDIR=$LFS install   && \
+echo "4. Make Install ..."                      && \
+make DESTDIR=$LFS install   \
+    > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR         && \
 \
 rm -v $LFS/usr/lib/lib{stdc++,stdc++fs,supc++}.la \
 \
@@ -30,4 +45,6 @@ rm -v $LFS/usr/lib/lib{stdc++,stdc++fs,supc++}.la \
 cd ..
 cd ..
 rm -rf $PKG
-unset PKG
+unset PKGLOG_ERROR PKGLOG_INSTALL
+unset PKGLOG_BUILD PKGLOG_CONFIG PKGLOG_TAR
+unset PKGLOG_DIR PKG
