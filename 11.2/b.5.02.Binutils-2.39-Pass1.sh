69ad2fa46ae85ff5b4136f007955ec09f93bc81e
sevillalarry <sevilla.larry.oss@gmail.com>
2022-11-12 10:09:57 +0800

    Remove Time Log

--- 11.2/b.5.02.Binutils-2.39-Pass1.sh
+++ 11.2/b.5.02.Binutils-2.39-Pass1.sh
@@ -8,7 +8,6 @@ export PKGLOG_CONFIG=$LFSLOG_CONFIG/$LOG
 export PKGLOG_BUILD=$LFSLOG_BUILD/$LOG
 export PKGLOG_INSTALL=$LFSLOG_INSTALL/$LOG
 export PKGLOG_ERROR=$LFSLOG_ERROR/$LOG
-export PKGLOG_TIME=$LFSLOG_TIME/$LOG
 
 echo "1. Extract tar..."
 tar xvf $PKG.tar.xz > $PKGLOG_TAR 2> $PKGLOG_ERROR
@@ -34,11 +33,11 @@ make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR          && \
 echo "4. Make Install ..."                      && \
 make install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR    \
 \
-; } > $PKGLOG_TIME 2>> $PKGLOG_ERROR
+; }
 
 cd ..
 cd ..
 rm -rf $PKG
-unset PKGLOG_TIME PKGLOG_ERROR PKGLOG_INSTALL
+unset PKGLOG_ERROR PKGLOG_INSTALL
 unset PKGLOG_BUILD PKGLOG_CONFIG PKGLOG_TAR
 unset PKG LOG
