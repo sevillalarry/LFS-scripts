e8fd3bb67ab17d6b83f7554d530ab1d3bdd8b446
sevillalarry <sevilla.larry.oss@gmail.com>
2022-11-12 10:54:52 +0800

    Linux Headers log

--- 11.2/b.5.04.Linux-5.19.2.API.Headers.sh
+++ 11.2/b.5.04.Linux-5.19.2.API.Headers.sh
@@ -1,20 +1,35 @@
 # b.5.04.Linux-5.19.2.API.Headers.sh
 #
 
+export LOG="5.04.Linux-5.19.2.API.Headers"
 export PKG="linux-5.15.76"
-tar xvf $PKG.tar.xz
+export PKGLOG_TAR=$LFSLOG_TAR/$LOG
+export PKGLOG_CONFIG=$LFSLOG_CONFIG/$LOG
+export PKGLOG_BUILD=$LFSLOG_BUILD/$LOG
+export PKGLOG_INSTALL=$LFSLOG_INSTALL/$LOG
+export PKGLOG_ERROR=$LFSLOG_ERROR/$LOG
+export PKGLOG_OTHERS=$LFSLOG_OTHERS/$LOG
+
+echo "1. Extract tar..."
+tar xvf $PKG.tar.xz > $PKGLOG_TAR 2> $PKGLOG_ERROR
 cd $PKG
 
 time { \
 \
-make mrproper                                   && \
+echo "2. Make MrProper ..."                         && \
+make mrproper > $PKGLOG_OTHERS 2>> $PKGLOG_ERROR    && \
 \
-make headers                                    && \
-find usr/include -type f ! -name '*.h' -delete  && \
-cp -rv usr/include $LFS/usr                     \
+echo "2. Make Headers ..."                          && \
+make headers >> $PKGLOG_OTHERS 2>> $PKGLOG_ERROR    && \
+find usr/include -type f ! -name '*.h' -delete      \
+             >> $PKGLOG_OTHERS 2>> $PKGLOG_ERROR    && \
+cp -rv usr/include $LFS/usr                         \
+             >> $PKGLOG_OTHERS 2>> $PKGLOG_ERROR    \
 \
 ; }
 
 cd ..
 rm -rf $PKG
-unset PKG
+unset PKGLOG_ERROR PKGLOG_INSTALL
+unset PKGLOG_BUILD PKGLOG_CONFIG PKGLOG_TAR
+unset PKG LOG
