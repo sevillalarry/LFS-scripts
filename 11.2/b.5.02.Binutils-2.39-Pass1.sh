2f8e7e6a5c519291797a077a3165a490ae7f5f99
sevillalarry <sevilla.larry.oss@gmail.com>
2022-11-12 09:41:44 +0800

    Add logs

--- 11.2/b.5.02.Binutils-2.39-Pass1.sh
+++ 11.2/b.5.02.Binutils-2.39-Pass1.sh
@@ -1,7 +1,14 @@
 # b.5.02.Binutils-2.39-Pass1.sh
 #
 
+export LOG="5.02.Binutils-2.39-Pass1"
 export PKG="binutils-2.39"
+export PKGLOG_CONFIG=$LFSLOG_CONFIG/$LOG
+export PKGLOG_BUILD=$LFSLOG_BUILD/$LOG
+export PKGLOG_INSTALL=$LFSLOG_INSTALL/$LOG
+export PKGLOG_ERROR=$LFSLOG_ERROR/$LOG
+export PKGLOG_TIME=$LFSLOG_TIME/$LOG
+
 tar xvf $PKG.tar.xz
 cd $PKG
 
@@ -10,18 +17,19 @@ time { \
 mkdir -v build && \
 cd       build && \
 \
-../configure --prefix=$LFS/tools \
-             --with-sysroot=$LFS \
-             --target=$LFS_TGT   \
-             --disable-nls       \
-             --enable-gprofng=no \
-             --disable-werror && \
+../configure --prefix=$LFS/tools    \
+             --with-sysroot=$LFS    \
+             --target=$LFS_TGT      \
+             --disable-nls          \
+             --enable-gprofng=no    \
+             --disable-werror       \
+             > $PKGLOG_CONFIG 2> $PKGLOG_ERROR  && \
 \
-make &&      \
+make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR  &&   \
 \
-make install \
+make install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR  \
 \
-; }
+; } > $PKGLOG_TIME 2>> $PKGLOG_ERROR
 
 cd ..
 cd ..
