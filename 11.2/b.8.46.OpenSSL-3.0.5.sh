bf08e8a4c1f8f7fce941c3d80b73e9e80d66b042
sevillalarry <sevilla.larry.oss@gmail.com>
2022-11-14 09:40:08 +0800

    Chapter 8 Logging batch 5

---
+++ 11.2/b.8.46.OpenSSL-3.0.5.sh
@@ -0,0 +1,51 @@
+# b.8.46.OpenSSL-3.0.5.sh
+#
+
+export PKG="openssl-3.0.5"
+export PKGLOG_DIR=$LFSLOG/8.46
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2> $PKGLOG_ERROR
+cd $PKG
+
+time { \
+\
+echo "2. Configure ..."            && \
+./config --prefix=/usr             \
+         --openssldir=/etc/ssl     \
+         --libdir=lib              \
+         shared                    \
+         zlib-dynamic              \
+         > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR  && \
+\
+echo "3. Make Build ..."                && \
+make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR  && \
+\
+echo "4. Make Check ..."                && \
+make test                               \
+     > $PKGLOG_CHECK 2>> $PKGLOG_ERROR  && \
+\
+echo "5. Make Install ..."              && \
+sed -i '/INSTALL_LIBS/s/libcrypto.a libssl.a//' Makefile    && \
+make MANSUFFIX=ssl install                   \
+     > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR     && \
+\
+mv -v /usr/share/doc/openssl /usr/share/doc/openssl-3.0.5   && \
+\
+cp -vfr doc/* /usr/share/doc/openssl-3.0.5   \
+\
+; }
+
+cd ..
+rm -rf $PKG
+unset PKGLOG_ERROR PKGLOG_INSTALL PKGLOG_CHECK
+unset PKGLOG_BUILD PKGLOG_CONFIG PKGLOG_TAR
+unset PKGLOG_DIR PKG
--- 11.2/b.8.49.Libffi-3.4.2.sh
+++ 11.2/b.8.49.Libffi-3.4.2.sh
@@ -1,8 +1,8 @@
-# b.8.45.Automake-1.16.5.sh
+# b.8.49.Libffi-3.4.2.sh
 #
 
-export PKG="automake-1.16.5"
-export PKGLOG_DIR=$LFSLOG/8.45
+export PKG="libffi-3.4.2"
+export PKGLOG_DIR=$LFSLOG/8.49
 export PKGLOG_TAR=$PKGLOG_DIR/tar.log
 export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
 export PKGLOG_BUILD=$PKGLOG_DIR/build.log
@@ -13,25 +13,28 @@ export PKGLOG_ERROR=$PKGLOG_DIR/error.log
 mkdir $PKGLOG_DIR
 
 echo "1. Extract tar..."
-tar xvf $PKG.tar.xz > $PKGLOG_TAR 2> $PKGLOG_ERROR
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2> $PKGLOG_ERROR
 cd $PKG
 
 time { \
 \
-echo "2. Configure ..."     && \
-./configure    --prefix=/usr   \
-               --docdir=/usr/share/doc/automake-1.16.5 \
-               > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR      && \
+echo "2. Configure ..."            && \
+./configure --prefix=/usr          \
+            --disable-static       \
+            --with-gcc-arch=native \
+            --disable-exec-static-tramp           \
+            > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR    && \
 \
 echo "3. Make Build ..."                && \
 make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR  && \
 \
 echo "4. Make Check ..."                && \
-make -j4 check                          \
+make check                              \
      > $PKGLOG_CHECK 2>> $PKGLOG_ERROR  && \
 \
 echo "5. Make Install ..."              && \
-make install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR    \
+make install                            \
+     > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR     \
 \
 ; }
 
