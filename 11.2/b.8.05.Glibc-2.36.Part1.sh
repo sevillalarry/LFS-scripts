bdab7813c61e792637d81f6e1ff19b6c13bc4be5
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-02-25 03:19:17 +0800

    split into two files

--- 11.2/b.8.05.Glibc-2.36.Part1.sh
+++ 11.2/b.8.05.Glibc-2.36.Part1.sh
@@ -1,5 +1,5 @@
 # b.8.05.Glibc-2.36.Part1.sh
-#
+# Extract, Configure, Build, Check
 
 export PKG="glibc-2.36"
 export PKGLOG_DIR=$LFSLOG/8.05
@@ -18,7 +18,7 @@ tar xvf $PKG.tar.xz > $PKGLOG_TAR 2> $PKGLOG_ERROR
 cd $PKG
 
 #Modify the next line for your local time zone
-export LOCAL_TIME_ZONE=Asia/Manila                                && \
+#export LOCAL_TIME_ZONE=Asia/Manila                                && \
 
 time { \
 \
@@ -43,92 +43,15 @@ echo "3. Make Build ..."                && \
 make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR  && \
 \
 echo "4. Make Check ..."                && \
-make check > $PKGLOG_CHECK 2>> $PKGLOG_ERROR        && \
-\
-touch /etc/ld.so.conf                   && \
-\
-sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile   && \
-\
-echo "5. Make Install ..."              && \
-make install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR    && \
-\
-sed '/RTLDLIST=/s@/usr@@g' -i /usr/bin/ldd  && \
-\
-cp -v ../nscd/nscd.conf /etc/nscd.conf      && \
-mkdir -pv /var/cache/nscd                   && \
-\
-echo "6. Locale Definitions ..."            && \
-mkdir -pv /usr/lib/locale                                       && \
-localedef -i POSIX -f UTF-8 C.UTF-8 2> /dev/null || true        && \
-localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8                         && \
-localedef -i de_DE -f ISO-8859-1 de_DE                          && \
-localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro               && \
-localedef -i de_DE -f UTF-8 de_DE.UTF-8                         && \
-localedef -i el_GR -f ISO-8859-7 el_GR                          && \
-localedef -i en_GB -f ISO-8859-1 en_GB                          && \
-localedef -i en_GB -f UTF-8 en_GB.UTF-8                         && \
-localedef -i en_HK -f ISO-8859-1 en_HK                          && \
-localedef -i en_PH -f ISO-8859-1 en_PH                          && \
-localedef -i en_US -f ISO-8859-1 en_US                          && \
-localedef -i en_US -f UTF-8 en_US.UTF-8                         && \
-localedef -i es_ES -f ISO-8859-15 es_ES@euro                    && \
-localedef -i es_MX -f ISO-8859-1 es_MX                          && \
-localedef -i fa_IR -f UTF-8 fa_IR                               && \
-localedef -i fr_FR -f ISO-8859-1 fr_FR                          && \
-localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro               && \
-localedef -i fr_FR -f UTF-8 fr_FR.UTF-8                         && \
-localedef -i is_IS -f ISO-8859-1 is_IS                          && \
-localedef -i is_IS -f UTF-8 is_IS.UTF-8                         && \
-localedef -i it_IT -f ISO-8859-1 it_IT                          && \
-localedef -i it_IT -f ISO-8859-15 it_IT@euro                    && \
-localedef -i it_IT -f UTF-8 it_IT.UTF-8                         && \
-localedef -i ja_JP -f EUC-JP ja_JP                              && \
-localedef -i ja_JP -f SHIFT_JIS ja_JP.SJIS 2> /dev/null || true && \
-localedef -i ja_JP -f UTF-8 ja_JP.UTF-8                         && \
-localedef -i nl_NL@euro -f ISO-8859-15 nl_NL@euro               && \
-localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R                       && \
-localedef -i ru_RU -f UTF-8 ru_RU.UTF-8                         && \
-localedef -i se_NO -f UTF-8 se_NO.UTF-8                         && \
-localedef -i ta_IN -f UTF-8 ta_IN.UTF-8                         && \
-localedef -i tr_TR -f UTF-8 tr_TR.UTF-8                         && \
-localedef -i zh_CN -f GB18030 zh_CN.GB18030                     && \
-localedef -i zh_HK -f BIG5-HKSCS zh_HK.BIG5-HKSCS               && \
-localedef -i zh_TW -f UTF-8 zh_TW.UTF-8                         && \
-\
-echo "7. Make Install Locales ..."                              && \
-make localedata/install-locales \
-    >> $PKGLOG_INSTALL 2>> $PKGLOG_ERROR && \
-\
-localedef -i POSIX -f UTF-8 C.UTF-8 2> /dev/null || true        && \
-localedef -i ja_JP -f SHIFT_JIS ja_JP.SJIS 2> /dev/null || true && \
-\
-echo "8. Setting Time Zone ..."     && \
-tar -xf ../../tzdata2022c.tar.gz    \
-    >> $PKGLOG_TAR 2> $PKGLOG_ERROR && \
-\
-ZONEINFO=/usr/share/zoneinfo        && \
-mkdir -pv $ZONEINFO/{posix,right}   && \
-\
-for tz in etcetera southamerica northamerica europe africa antarctica  \
-          asia australasia backward; do
-    zic -L /dev/null   -d $ZONEINFO       ${tz}
-    zic -L /dev/null   -d $ZONEINFO/posix ${tz}
-    zic -L leapseconds -d $ZONEINFO/right ${tz}
-done      && \
-\
-cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO     && \
-zic -d $ZONEINFO -p $LOCAL_TIME_ZONE                  && \
-unset ZONEINFO                                        && \
-\
-ln -sfv /usr/share/zoneinfo/$LOCAL_TIME_ZONE /etc/localtime   \
+make check > $PKGLOG_CHECK 2>> $PKGLOG_ERROR        \
 \
 ; }
 
-unset LOCAL_TIME_ZONE
+#unset LOCAL_TIME_ZONE
 
 cd ..
 cd ..
-rm -rf $PKG
+#rm -rf $PKG
 unset PKGLOG_CHECK
 unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
 unset PKGLOG_ERROR PKGLOG_TAR
