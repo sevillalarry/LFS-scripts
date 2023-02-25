42ad6b539813056700a975783084582a4d0c784e
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-02-25 21:52:10 +0800

    split into 2 files

--- 11.2/b.8.67.Tar-1.34.Part2.sh
+++ 11.2/b.8.67.Tar-1.34.Part2.sh
@@ -1,5 +1,5 @@
-# b.8.67.Tar-1.34.sh
-#
+# b.8.67.Tar-1.34.Part2.sh
+# Install
 
 export PKG="tar-1.34"
 export PKGLOG_DIR=$LFSLOG/8.67
@@ -10,30 +10,17 @@ export PKGLOG_CHECK=$PKGLOG_DIR/check.log
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
 
-rm -r $PKGLOG_DIR 2> /dev/null
-mkdir $PKGLOG_DIR
+#rm -r $PKGLOG_DIR 2> /dev/null
+#mkdir $PKGLOG_DIR
 
-echo "1. Extract tar..."
-tar xvf $PKG.tar.xz > $PKGLOG_TAR 2> $PKGLOG_ERROR
 cd $PKG
 
 time { \
 \
-echo "2. Configure ..."                      && \
-FORCE_UNSAFE_CONFIGURE=1                     \
-./configure    --prefix=/usr                 \
-     > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR      && \
-\
-echo "3. Make Build ..."                     && \
-make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR       && \
-\
-echo "4. Make Check ..."                     && \
-make check                                   \
-     > $PKGLOG_CHECK 2>> $PKGLOG_ERROR       && \
-\
 echo "5. Make Install ..."                   && \
 make install                                 \
       > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR    && \
+\
 make -C doc install-html                     \
      docdir=/usr/share/doc/tar-1.34          \
      >> $PKGLOG_INSTALL 2>> $PKGLOG_ERROR    \
