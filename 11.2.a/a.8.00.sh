66f7de6de585b3ac74c67da83e3490abc55b52b4
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-03-01 09:14:10 +0800

    file update

--- 11.2.a/a.8.00.sh
+++ 11.2.a/a.8.00.sh
@@ -386,12 +386,590 @@ echo "." >> LFSLOG_TIME
 
 ###
 
+echo "8.26.GCC-12.2.0.Part1"
+echo "8.26.GCC-12.2.0.Part1" >> LFSLOG_PROCESS
+echo "8.26.GCC-12.2.0.Part1" >> LFSLOG_TIME
+
+time {
+    bash a.8.26.GCC-12.2.0.Part1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.26.GCC-12.2.0.Part2"
+echo "8.26.GCC-12.2.0.Part2" >> LFSLOG_PROCESS
+echo "8.26.GCC-12.2.0.Part2" >> LFSLOG_TIME
+
+time {
+    bash a.8.26.GCC-12.2.0.Part2.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.26.GCC-12.2.0.Part3"
+echo "8.26.GCC-12.2.0.Part3" >> LFSLOG_PROCESS
+echo "8.26.GCC-12.2.0.Part3" >> LFSLOG_TIME
+
+time {
+    bash a.8.26.GCC-12.2.0.Part3.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.26.GCC-12.2.0.Part4"
+echo ""8.26.GCC-12.2.0.Part4 >> LFSLOG_PROCESS
+echo "8.26.GCC-12.2.0.Part4" >> LFSLOG_TIME
+
+time {
+    bash a.8.26.GCC-12.2.0.Part4.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.27.Pkg-config-0.29.2"
+echo "8.27.Pkg-config-0.29.2" >> LFSLOG_PROCESS
+echo "8.27.Pkg-config-0.29.2" >> LFSLOG_TIME
+
+time {
+    bash a.8.27.Pkg-config-0.29.2.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.28.Ncurses-6.3"
+echo "8.28.Ncurses-6.3" >> LFSLOG_PROCESS
+echo "8.28.Ncurses-6.3" >> LFSLOG_TIME
+
+time {
+    bash a.8.28.Ncurses-6.3.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.29.Sed-4.8"
+echo "8.29.Sed-4.8" >> LFSLOG_PROCESS
+echo "8.29.Sed-4.8" >> LFSLOG_TIME
+
+time {
+    bash a.8.29.Sed-4.8.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.30.Psmisc-23.5"
+echo "8.30.Psmisc-23.5" >> LFSLOG_PROCESS
+echo "8.30.Psmisc-23.5" >> LFSLOG_TIME
+
+time {
+    bash a.8.30.Psmisc-23.5.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.31.Gettext-0.21"
+echo "8.31.Gettext-0.21" >> LFSLOG_PROCESS
+echo "8.31.Gettext-0.21" >> LFSLOG_TIME
+
+time {
+    bash a.8.31.Gettext-0.21.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.32.Bison-3.8.2"
+echo "8.32.Bison-3.8.2" >> LFSLOG_PROCESS
+echo "8.32.Bison-3.8.2" >> LFSLOG_TIME
+
+time {
+    bash a.8.32.Bison-3.8.2.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.33.Grep-3.7"
+echo "8.33.Grep-3.7" >> LFSLOG_PROCESS
+echo "8.33.Grep-3.7" >> LFSLOG_TIME
+
+time {
+    bash a.8.33.Grep-3.7.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.34.Bash-5.1.16"
+echo "8.34.Bash-5.1.16" >> LFSLOG_PROCESS
+echo "8.34.Bash-5.1.16" >> LFSLOG_TIME
+
+time {
+    bash a.8.34.Bash-5.1.16.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.35.Libtool-2.4.7.Part1"
+echo "8.35.Libtool-2.4.7.Part1" >> LFSLOG_PROCESS
+echo "8.35.Libtool-2.4.7.Part1" >> LFSLOG_TIME
+
+time {
+    bash a.8.35.Libtool-2.4.7.Part1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.35.Libtool-2.4.7.Part2"
+echo "8.35.Libtool-2.4.7.Part2" >> LFSLOG_PROCESS
+echo "8.35.Libtool-2.4.7.Part2" >> LFSLOG_TIME
+
+time {
+    bash a.8.35.Libtool-2.4.7.Part2.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.36.GDBM-1.23"
+echo "8.36.GDBM-1.23" >> LFSLOG_PROCESS
+echo "8.36.GDBM-1.23" >> LFSLOG_TIME
+
+time {
+    bash a.8.36.GDBM-1.23.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.37.Gperf-3.1"
+echo "8.37.Gperf-3.1" >> LFSLOG_PROCESS
+echo "8.37.Gperf-3.1" >> LFSLOG_TIME
+
+time {
+    bash a.8.37.Gperf-3.1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.38.Expat-2.5.0"
+echo "8.38.Expat-2.5.0" >> LFSLOG_PROCESS
+echo "8.38.Expat-2.5.0" >> LFSLOG_TIME
+
+time {
+    bash a.8.38.Expat-2.5.0.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.39.Inetutils-2.4"
+echo "8.39.Inetutils-2.4" >> LFSLOG_PROCESS
+echo "8.39.Inetutils-2.4" >> LFSLOG_TIME
+
+time {
+    bash a.8.39.Inetutils-2.4.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.40.Less-590"
+echo "8.40.Less-590" >> LFSLOG_PROCESS
+echo "8.40.Less-590" >> LFSLOG_TIME
+
+time {
+    bash a.8.40.Less-590.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.41.Perl-5.36.0"
+echo "8.41.Perl-5.36.0" >> LFSLOG_PROCESS
+echo "8.41.Perl-5.36.0" >> LFSLOG_TIME
+
+time {
+    bash a.8.41.Perl-5.36.0.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.42.XML-Parser-2.46"
+echo "8.42.XML-Parser-2.46" >> LFSLOG_PROCESS
+echo "8.42.XML-Parser-2.46" >> LFSLOG_TIME
+
+time {
+    bash a.8.42.XML-Parser-2.46.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.43.Intltool-0.51.0"
+echo "8.43.Intltool-0.51.0" >> LFSLOG_PROCESS
+echo "8.43.Intltool-0.51.0" >> LFSLOG_TIME
+
+time {
+    bash a.8.43.Intltool-0.51.0.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
 echo ""
 echo "" >> LFSLOG_PROCESS
 echo "" >> LFSLOG_TIME
 
 time {
     bash a..sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.44.Autoconf-2.71"
+echo "8.44.Autoconf-2.71" >> LFSLOG_PROCESS
+echo "8.44.Autoconf-2.71" >> LFSLOG_TIME
+
+time {
+    bash a.8.44.Autoconf-2.71.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.45.Automake-1.16.5"
+echo "8.45.Automake-1.16.5" >> LFSLOG_PROCESS
+echo "8.45.Automake-1.16.5" >> LFSLOG_TIME
+
+time {
+    bash a.8.45.Automake-1.16.5.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.46.OpenSSL-3.0.8"
+echo "8.46.OpenSSL-3.0.8" >> LFSLOG_PROCESS
+echo "8.46.OpenSSL-3.0.8" >> LFSLOG_TIME
+
+time {
+    bash a.8.46.OpenSSL-3.0.8.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.47.Kmod-30"
+echo "8.47.Kmod-30" >> LFSLOG_PROCESS
+echo "8.47.Kmod-30" >> LFSLOG_TIME
+
+time {
+    bash a.8.47.Kmod-30.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.48.Libelf.from.Elfutils-0.187"
+echo "8.48.Libelf.from.Elfutils-0.187" >> LFSLOG_PROCESS
+echo "8.48.Libelf.from.Elfutils-0.187" >> LFSLOG_TIME
+
+time {
+    bash a.8.48.Libelf.from.Elfutils-0.187.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.49.Libffi-3.4.2"
+echo "8.49.Libffi-3.4.2" >> LFSLOG_PROCESS
+echo "8.49.Libffi-3.4.2" >> LFSLOG_TIME
+
+time {
+    bash a.8.49.Libffi-3.4.2.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.50.Python-3.10.9"
+echo "8.50.Python-3.10.9" >> LFSLOG_PROCESS
+echo "8.50.Python-3.10.9" >> LFSLOG_TIME
+
+time {
+    bash a.8.50.Python-3.10.9.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.51.Wheel-0.37.1"
+echo "8.51.Wheel-0.37.1" >> LFSLOG_PROCESS
+echo "8.51.Wheel-0.37.1" >> LFSLOG_TIME
+
+time {
+    bash a.8.51.Wheel-0.37.1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.52.Ninja-1.11.0"
+echo "8.52.Ninja-1.11.0" >> LFSLOG_PROCESS
+echo "8.52.Ninja-1.11.0" >> LFSLOG_TIME
+
+time {
+    bash a.8.52.Ninja-1.11.0.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.53.Meson-0.63.1"
+echo "8.53.Meson-0.63.1" >> LFSLOG_PROCESS
+echo "8.53.Meson-0.63.1" >> LFSLOG_TIME
+
+time {
+    bash a.8.53.Meson-0.63.1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.54.Coreutils-9.1.Part1"
+echo "8.54.Coreutils-9.1.Part1" >> LFSLOG_PROCESS
+echo "8.54.Coreutils-9.1.Part1" >> LFSLOG_TIME
+
+time {
+    bash a.8.54.Coreutils-9.1.Part1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.54.Coreutils-9.1.Part3"
+echo "8.54.Coreutils-9.1.Part3" >> LFSLOG_PROCESS
+echo "8.54.Coreutils-9.1.Part3" >> LFSLOG_TIME
+
+time {
+    bash a.8.54.Coreutils-9.1.Part3.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.55.Check-0.15.2"
+echo "8.55.Check-0.15.2" >> LFSLOG_PROCESS
+echo "8.55.Check-0.15.2" >> LFSLOG_TIME
+
+time {
+    bash a.8.55.Check-0.15.2.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.56.Diffutils-3.8"
+echo "8.56.Diffutils-3.8" >> LFSLOG_PROCESS
+echo "8.56.Diffutils-3.8" >> LFSLOG_TIME
+
+time {
+    bash a.8.56.Diffutils-3.8.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.57.Gawk-5.1.1"
+echo "8.57.Gawk-5.1.1" >> LFSLOG_PROCESS
+echo "8.57.Gawk-5.1.1" >> LFSLOG_TIME
+
+time {
+    bash a.8.57.Gawk-5.1.1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.58.Findutils-4.9.0"
+echo "8.58.Findutils-4.9.0" >> LFSLOG_PROCESS
+echo "8.58.Findutils-4.9.0" >> LFSLOG_TIME
+
+time {
+    bash a.8.58.Findutils-4.9.0.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.59.Groff-1.22.4"
+echo "8.59.Groff-1.22.4" >> LFSLOG_PROCESS
+echo "8.59.Groff-1.22.4" >> LFSLOG_TIME
+
+time {
+    bash a.8.59.Groff-1.22.4.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.60.GRUB-2.06.for.EFI.x64"
+echo "8.60.GRUB-2.06.for.EFI.x64" >> LFSLOG_PROCESS
+echo "8.60.GRUB-2.06.for.EFI.x64" >> LFSLOG_TIME
+
+time {
+    bash a.8.60.GRUB-2.06.for.EFI.x64.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.60A.Popt-1.18"
+echo "8.60A.Popt-1.18" >> LFSLOG_PROCESS
+echo "8.60A.Popt-1.18" >> LFSLOG_TIME
+
+time {
+    bash a.8.60A.Popt-1.18.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.60B.mandoc-1.14.6"
+echo "8.60B.mandoc-1.14.6" >> LFSLOG_PROCESS
+echo "8.60B.mandoc-1.14.6" >> LFSLOG_TIME
+
+time {
+    bash a.8.60B.mandoc-1.14.6.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.60C.efivar-38.Part1"
+echo "8.60C.efivar-38.Part1" >> LFSLOG_PROCESS
+echo "8.60C.efivar-38.Part1" >> LFSLOG_TIME
+
+time {
+    bash a.8.60C.efivar-38.Part1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.60C.efivar-38.Part2"
+echo "8.60C.efivar-38.Part2" >> LFSLOG_PROCESS
+echo "8.60C.efivar-38.Part2" >> LFSLOG_TIME
+
+time {
+    bash a.8.60C.efivar-38.Part2.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.60D.efibootmgr-18.Part1"
+echo "8.60D.efibootmgr-18.Part1" >> LFSLOG_PROCESS
+echo "8.60D.efibootmgr-18.Part1" >> LFSLOG_TIME
+
+time {
+    bash a.8.60D.efibootmgr-18.Part1.sh
 ;} >> LFSLOG_TIME
 
 echo "." >> LFSLOG_PROCESS
@@ -412,12 +990,12 @@ echo "." >> LFSLOG_TIME
 
 ###
 
-echo ""
-echo "" >> LFSLOG_PROCESS
-echo "" >> LFSLOG_TIME
+echo "8.60D.efibootmgr-18.Part2"
+echo "8.60D.efibootmgr-18.Part2" >> LFSLOG_PROCESS
+echo "8.60D.efibootmgr-18.Part2" >> LFSLOG_TIME
 
 time {
-    bash a..sh
+    bash a.8.60D.efibootmgr-18.Part2.sh
 ;} >> LFSLOG_TIME
 
 echo "." >> LFSLOG_PROCESS
@@ -425,12 +1003,12 @@ echo "." >> LFSLOG_TIME
 
 ###
 
-echo ""
-echo "" >> LFSLOG_PROCESS
-echo "" >> LFSLOG_TIME
+echo "8.60Y.GRUB-2.06.for.EFI.x64"
+echo "8.60Y.GRUB-2.06.for.EFI.x64" >> LFSLOG_PROCESS
+echo "8.60Y.GRUB-2.06.for.EFI.x64" >> LFSLOG_TIME
 
 time {
-    bash a..sh
+    bash a.8.60Y.GRUB-2.06.for.EFI.x64.sh
 ;} >> LFSLOG_TIME
 
 echo "." >> LFSLOG_PROCESS
@@ -438,18 +1016,251 @@ echo "." >> LFSLOG_TIME
 
 ###
 
-echo ""
-echo "" >> LFSLOG_PROCESS
-echo "" >> LFSLOG_TIME
+echo "8.61.Gzip-1.12"
+echo "8.61.Gzip-1.12" >> LFSLOG_PROCESS
+echo "8.61.Gzip-1.12" >> LFSLOG_TIME
 
 time {
-    bash a..sh
+    bash a.8.61.Gzip-1.12.sh
 ;} >> LFSLOG_TIME
 
 echo "." >> LFSLOG_PROCESS
 echo "." >> LFSLOG_TIME
 
-#####
+###
+
+echo "8.62.IPRoute2-5.19.0"
+echo "8.62.IPRoute2-5.19.0" >> LFSLOG_PROCESS
+echo "8.62.IPRoute2-5.19.0" >> LFSLOG_TIME
+
+time {
+    bash a.8.62.IPRoute2-5.19.0.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.63.Kbd-2.5.1"
+echo "8.63.Kbd-2.5.1" >> LFSLOG_PROCESS
+echo "8.63.Kbd-2.5.1" >> LFSLOG_TIME
+
+time {
+    bash a.8.63.Kbd-2.5.1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.64.Libpipeline-1.5.6"
+echo "8.64.Libpipeline-1.5.6" >> LFSLOG_PROCESS
+echo "8.64.Libpipeline-1.5.6" >> LFSLOG_TIME
+
+time {
+    bash a.8.64.Libpipeline-1.5.6.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.65.Make-4.3"
+echo "8.65.Make-4.3" >> LFSLOG_PROCESS
+echo "8.65.Make-4.3" >> LFSLOG_TIME
+
+time {
+    bash a.8.65.Make-4.3.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.66.Patch-2.7.6"
+echo "8.66.Patch-2.7.6" >> LFSLOG_PROCESS
+echo "8.66.Patch-2.7.6" >> LFSLOG_TIME
+
+time {
+    bash a.8.66.Patch-2.7.6.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
+###
+
+echo "8.67.Tar-1.34.Part1"
+echo "8.67.Tar-1.34.Part1" >> LFSLOG_PROCESS
+echo "8.67.Tar-1.34.Part1" >> LFSLOG_TIME
+
+time {
+    bash a.8.67.Tar-1.34.Part1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.67.Tar-1.34.Part2"
+echo "8.67.Tar-1.34.Part2" >> LFSLOG_PROCESS
+echo "8.67.Tar-1.34.Part2" >> LFSLOG_TIME
+
+time {
+    bash a.8.67.Tar-1.34.Part2.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.68.Texinfo-6.8"
+echo "8.68.Texinfo-6.8" >> LFSLOG_PROCESS
+echo "8.68.Texinfo-6.8" >> LFSLOG_TIME
+
+time {
+    bash a.8.68.Texinfo-6.8.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.69a.Nano-6.4"
+echo "8.69a.Nano-6.4" >> LFSLOG_PROCESS
+echo "8.69a.Nano-6.4" >> LFSLOG_TIME
+
+time {
+    bash a.8.69a.Nano-6.4.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.70.Eudev-3.2.11"
+echo "8.70.Eudev-3.2.11" >> LFSLOG_PROCESS
+echo "8.70.Eudev-3.2.11" >> LFSLOG_TIME
+
+time {
+    bash a.8.70.Eudev-3.2.11.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.71.Man-DB-2.10.2"
+echo "8.71.Man-DB-2.10.2" >> LFSLOG_PROCESS
+echo "8.71.Man-DB-2.10.2" >> LFSLOG_TIME
+
+time {
+    bash a.8.71.Man-DB-2.10.2.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.72.Procps-ng-4.0.0"
+echo "8.72.Procps-ng-4.0.0" >> LFSLOG_PROCESS
+echo "8.72.Procps-ng-4.0.0" >> LFSLOG_TIME
+
+time {
+    bash a.8.72.Procps-ng-4.0.0.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.73.Util-linux-2.38.1.Part1"
+echo "8.73.Util-linux-2.38.1.Part1" >> LFSLOG_PROCESS
+echo "8.73.Util-linux-2.38.1.Part1" >> LFSLOG_TIME
+
+time {
+    bash a.8.73.Util-linux-2.38.1.Part1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.73.Util-linux-2.38.1.Part2"
+echo "8.73.Util-linux-2.38.1.Part2" >> LFSLOG_PROCESS
+echo "8.73.Util-linux-2.38.1.Part2" >> LFSLOG_TIME
+
+time {
+    bash a.8.73.Util-linux-2.38.1.Part2.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.73.Util-linux-2.38.1.Part3"
+echo "8.73.Util-linux-2.38.1.Part3" >> LFSLOG_PROCESS
+echo "8.73.Util-linux-2.38.1.Part3" >> LFSLOG_TIME
+
+time {
+    bash a.8.73.Util-linux-2.38.1.Part3.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.74.E2fsprogs-1.46.6"
+echo "8.74.E2fsprogs-1.46.6" >> LFSLOG_PROCESS
+echo "8.74.E2fsprogs-1.46.6" >> LFSLOG_TIME
+
+time {
+    bash a.8.74.E2fsprogs-1.46.6.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.75.Sysklogd-1.5.1"
+echo "8.75.Sysklogd-1.5.1" >> LFSLOG_PROCESS
+echo "8.75.Sysklogd-1.5.1" >> LFSLOG_TIME
+
+time {
+    bash a.8.75.Sysklogd-1.5.1.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.76.Sysvinit-3.04"
+echo "8.76.Sysvinit-3.04" >> LFSLOG_PROCESS
+echo "8.76.Sysvinit-3.04" >> LFSLOG_TIME
+
+time {
+    bash a.8.76.Sysvinit-3.04.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+###
+
+echo "8.78.&.79.Stripping.&.Cleaning.Up"
+echo "8.78.&.79.Stripping.&.Cleaning.Up" >> LFSLOG_PROCESS
+echo "8.78.&.79.Stripping.&.Cleaning.Up" >> LFSLOG_TIME
+
+time {
+    bash a.8.78.&.79.Stripping.&.Cleaning.Up.sh
+;} >> LFSLOG_TIME
+
+echo "." >> LFSLOG_PROCESS
+echo "." >> LFSLOG_TIME
+
 
 unset LFSLOG_TIME
 unset LFSLOG_PROCESS
