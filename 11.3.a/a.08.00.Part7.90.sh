6bf30288b63afdb7e0ea16ada75c011be4178d0b
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-05-10 16:31:54 +0800

    bug fix

--- 11.3.a/a.08.00.Part7.90.sh
+++ 11.3.a/a.08.00.Part7.90.sh
@@ -12,7 +12,7 @@ echo "7.13.pause.Backup"
 echo "7.13.pause.Backup" >> $LFSLOG_PROCESS
 
 bash a.07.13.pause.Backup.sh
-bash a.08.90.for.78.n.79.Stripping.n.Cleaning.Up
+bash a.08.90.for.78.n.79.Stripping.n.Cleaning.Up.sh
 echo "." >> $LFSLOG_PROCESS
 echo "."
 echo "."
