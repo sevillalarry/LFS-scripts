4725cc4a9b55ad18ab10a59c34904ebf10f47c2a
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-08-06 16:39:52 +0800

    2023/Aug/06 16:40

--- 11.3.a/a.08.25.Shadow-4.13.Part1.sh
+++ 11.3.a/a.08.25.Shadow-4.13.Part1.sh
@@ -6,7 +6,7 @@ export PKGLOG_DIR=$LFSLOG/08.25
 export PKGLOG_TAR=$PKGLOG_DIR/tar.log
 export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
 export PKGLOG_BUILD=$PKGLOG_DIR/build.log
-export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+#export PKGLOG_CHECK=$PKGLOG_DIR/check.log
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
 export LFSLOG_PROCESS=$LFSLOG/process.log
@@ -58,7 +58,7 @@ make -C man install-man         \
 cd ..
 rm -rf $PKG
 unset LFSLOG_PROCESS
-unset PKGLOG_CHECK
+#unset PKGLOG_CHECK
 unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
 unset PKGLOG_ERROR PKGLOG_TAR
 unset PKGLOG_DIR PKG
