ac0445d5af437ac2a135fbe01c44e3dd5700eee2
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-10-01 12:41:54 +0800

    typo fix on log directory

--- 12.0.a/a.07.09.Perl-5.38.0.sh
+++ 12.0.a/a.07.09.Perl-5.38.0.sh
@@ -2,7 +2,7 @@
 #
 
 export PKG="perl-5.38.0"
-export PKGLOG_DIR=$LFSLOG/07.08
+export PKGLOG_DIR=$LFSLOG/07.09
 export PKGLOG_TAR=$PKGLOG_DIR/tar.log
 export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
 export PKGLOG_BUILD=$PKGLOG_DIR/build.log
