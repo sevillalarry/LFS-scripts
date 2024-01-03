09b2e61a7c670d0c85d58f52232c2ba401aa3881
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2024-01-03 11:58:37 +0800

    Perl errata

--- 12.0.a/a.07.09.Perl-5.38.2.sh
+++ 12.0.a/a.07.09.Perl-5.38.2.sh
@@ -1,14 +1,15 @@
-# a.08.42.Perl-5.38.0.sh
+# a.07.09.Perl-5.38.2.sh
+# errata
 #
 
-export PKG="perl-5.38.0"
-export PKGLOG_DIR=$LFSLOG/08.42
+export PKG="perl-5.38.2"
+export PKGLOG_DIR=$LFSLOG/07.09
 export PKGLOG_TAR=$PKGLOG_DIR/tar.log
 export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
 export PKGLOG_BUILD=$PKGLOG_DIR/build.log
-export PKGLOG_CHECK=$PKGLOG_DIR/check.log
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export LFSLOG_PROCESS=$LFSLOG/process.log
 
 rm -r $PKGLOG_DIR 2> /dev/null
 mkdir $PKGLOG_DIR
@@ -20,26 +21,19 @@ tar xvf $PKG.tar.xz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
 cd $PKG
 
 
-export BUILD_ZLIB=False
-export BUILD_BZIP2=0
-
 echo "2. Configure ..."
 echo "2. Configure ..." >> $LFSLOG_PROCESS
 echo "2. Configure ..." >> $PKGLOG_ERROR
 sh Configure -des                                           \
              -Dprefix=/usr                                  \
              -Dvendorprefix=/usr                            \
+             -Duseshrplib                                   \
              -Dprivlib=/usr/lib/perl5/5.38/core_perl        \
              -Darchlib=/usr/lib/perl5/5.38/core_perl        \
              -Dsitelib=/usr/lib/perl5/5.38/site_perl        \
              -Dsitearch=/usr/lib/perl5/5.38/site_perl       \
              -Dvendorlib=/usr/lib/perl5/5.38/vendor_perl    \
              -Dvendorarch=/usr/lib/perl5/5.38/vendor_perl   \
-             -Dman1dir=/usr/share/man/man1                  \
-             -Dman3dir=/usr/share/man/man3                  \
-             -Dpager="/usr/bin/less -isR"                   \
-             -Duseshrplib                                   \
-             -Dusethreads                                   \
              > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
 
 echo "3. Make Build ..."
@@ -47,23 +41,15 @@ echo "3. Make Build ..." >> $LFSLOG_PROCESS
 echo "3. Make Build ..." >> $PKGLOG_ERROR
 make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
 
-echo "4. Make Check ..."
-echo "4. Make Check ..." >> $LFSLOG_PROCESS
-echo "4. Make Check ..." >> $PKGLOG_ERROR
-make test > $PKGLOG_CHECK 2>> $PKGLOG_ERROR
-
-echo "5. Make Install ..."
-echo "5. Make Install ..." >> $LFSLOG_PROCESS
-echo "5. Make Install ..." >> $PKGLOG_ERROR
+echo "4. Make Install ..."
+echo "4. Make Install ..." >> $LFSLOG_PROCESS
+echo "4. Make Install ..." >> $PKGLOG_ERROR
 make install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
-
-unset BUILD_ZLIB BUILD_BZIP2
 
 
 cd ..
 rm -rf $PKG
 unset LFSLOG_PROCESS
-unset PKGLOG_CHECK
 unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
 unset PKGLOG_ERROR PKGLOG_TAR
 unset PKGLOG_DIR PKG
