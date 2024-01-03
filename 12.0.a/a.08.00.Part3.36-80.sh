d7d2a57bab697ee328e8eae5f857b0973aa53659
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2024-01-03 11:48:19 +0800

    OpenSSL errata

--- 12.0.a/a.08.00.Part3.36-80.sh
+++ 12.0.a/a.08.00.Part3.36-80.sh
@@ -162,10 +162,10 @@ echo "."
 
 ###
 
-echo "08.47.OpenSSL-3.1.4"
-echo "08.47.OpenSSL-3.1.4" >> $LFSLOG_PROCESS
+echo "08.47.OpenSSL-3.2.0"
+echo "08.47.OpenSSL-3.2.0" >> $LFSLOG_PROCESS
 
-time { bash a.08.47.OpenSSL-3.1.4.sh ; }
+time { bash a.08.47.OpenSSL-3.2.0.sh ; }
 
 date +"%Y/%b/%d %H:%M"
 date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
